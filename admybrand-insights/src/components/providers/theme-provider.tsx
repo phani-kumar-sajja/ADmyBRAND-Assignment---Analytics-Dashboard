f075b72576fdad616c952eabbc8c86100064f4f6
sajja.phani kumar <140970733+phani-kumar-sajja@users.noreply.github.com>
2025-07-28 23:33:37 +0530

    Update theme-provider.tsx
    Updated theme provider

--- admybrand-insights/src/components/providers/theme-provider.tsx
+++ admybrand-insights/src/components/providers/theme-provider.tsx
@@ -5,6 +5,17 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+};
