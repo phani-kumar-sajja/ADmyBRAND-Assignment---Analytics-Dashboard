3e4a4af0fb292aaa4748ed53844fb9b27c6320d7
sajja.phani kumar <140970733+phani-kumar-sajja@users.noreply.github.com>
2025-07-28 23:55:14 +0530

    Update theme-provider.tsx

--- admybrand-insights/src/components/providers/theme-provider.tsx
+++ admybrand-insights/src/components/providers/theme-provider.tsx
@@ -1,9 +1,8 @@
 // src/components/providers/theme-provider.tsx
 "use client"
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
   children,
@@ -11,11 +10,3 @@ export function ThemeProvider({
 }: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
-
-type ThemeProviderProps = {
-  children: React.ReactNode;
-  attribute?: string;
-  defaultTheme?: string;
-  enableSystem?: boolean;
-  disableTransitionOnChange?: boolean;
-};
